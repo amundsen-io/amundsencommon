3665f6fd98596feddb375553982e12f900a40856
Dmitriy Kunitskiy <dkunitskiy@lyft.com>
2021-03-09 16:42:20 -0800

    fix: standardize requirements and fixes for marshmallow3+
    Signed-off-by: Dmitriy Kunitskiy <dkunitskiy@lyft.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import find_packages, setup
 
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 setup(
     name='amundsen-common',
@@ -15,10 +15,6 @@ setup(
     maintainer='Amundsen TSC',
     maintainer_email='amundsen-tsc@lists.lfai.foundation',
     packages=find_packages(exclude=['tests*']),
-    dependency_links=[
-        ('git+https://www.github.com/hilearn/marshmallow-annotations.git@a7a2dc96932430369bd'
-         'ef36555082df990ed9bef#egg=marshmallow-annotations')
-    ],
     install_requires=[
         # Packages in here should rarely be pinned. This is because these
         # packages (at the specified version) are required for project
@@ -35,9 +31,10 @@ setup(
         # This will allow for any consuming projects to use this library as
         # long as they have a version of pyfoobar equal to or greater than 1.x
         # and less than 2.x installed.
-        'flask>=1.0.2',
+        'Flask>=1.0.2',
+        'attrs>=19.0.0',
         'marshmallow>=3.0,<=3.6',
-        'marshmallow-annotations'
+        'marshmallow-annotations @ git+https://www.github.com/hilearn/marshmallow-annotations.git@a7a2dc96932430369bdef36555082df990ed9bef#egg=marshmallow-annotations-2.4.0.post1'
     ],
     python_requires=">=3.6",
     package_data={'amundsen_common': ['py.typed']},
