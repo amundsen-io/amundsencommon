d523112a22ad201418fec15281e3d647728c16e1
Junda Yang <youngyjd@gmail.com>
2021-04-01 10:23:07 -0700

    update

--- amundsen_common/models/data_quality_checks.py
+++ amundsen_common/models/data_quality_checks.py
@@ -1,15 +1,29 @@
 # Copyright Contributors to the Amundsen project.
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import List, Optional
+
 import attr
 from marshmallow3_annotations.ext.attrs import AttrsSchema
 
 
 @attr.s(auto_attribs=True, kw_only=True)
+class TableQualityCheckRun:
+    run_id: str = attr.ib()
+    execution_status: str = attr.ib()
+    execution_timestamp: int = attr.ib()
+
+
+class TableQualityCheckRunSchema(AttrsSchema):
+    class Meta:
+        target = TableQualityCheckRun
+        register_as_scheme = True
+
+
+@attr.s(auto_attribs=True, kw_only=True)
 class TableQualityCheck:
     check_id: str = attr.ib()
-    last_execution_status: str = attr.ib()
-    last_execution_timestamp: int = attr.ib()
+    runs: Optional[List[TableQualityCheckRun]] = []
 
 
 class TableQualityCheckSchema(AttrsSchema):
